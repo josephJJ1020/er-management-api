8c4bb0644b2af67ca99f6229976c4e3196b3e24b
Earl <earl@bluehive.com.ph>
2026-03-31 18:37:06 +0800

    add resources for records module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientsModule } from './patients/patients.module';
+import { RecordsModule } from './records/records.module';
 
 @Module({
-  imports: [PatientsModule],
+  imports: [PatientsModule, RecordsModule],
   controllers: [AppController],
   providers: [AppService],
 })
